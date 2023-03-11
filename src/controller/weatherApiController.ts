e8cd97a1328e16936e272a1f70847a0b1734a64b
Lennart <115736797+lnart@users.noreply.github.com>
2023-03-11 22:48:32 +0100

    write function getWeatherData from API

---
+++ src/controller/weatherApiController.ts
@@ -0,0 +1,18 @@
+import get from 'https'
+
+async function getWeatherData() {
+    try{
+        let response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Wermelskirchen/2023-02-11/2023-03-11?unitGroup=metric&include=days%2Ccurrent&key=NP2FELSQW3GM3CKAP3EQQPJHZ&contentType=json');
+        if(!response.ok){
+            throw new Error(`HTTP Error! Status: ${response.status}`);
+        }
+        let data = await response.json()
+        console.log(data);
+        return data
+    }catch(error){
+        console.error(error)
+        return null
+    }
+}
+
+getWeatherData()
