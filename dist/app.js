d4a8692267cd92820fc32e8e1239c65cf6190ef7
Lennart <115736797+lnart@users.noreply.github.com>
2023-03-13 12:42:53 +0100

    import and use routes

--- dist/app.js
+++ dist/app.js
@@ -5,13 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = require("dotenv");
+const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.set('views', 'src/views');
 app.set('view engine', 'ejs');
-app.use('src/public', express_1.default.static('public'));
+app.use('/src/public', express_1.default.static('public'));
+app.use('/', indexRoutes_1.default);
 app.listen(process.env.PORT, () => {
     console.log(`STARTED SERVER ON PORT ${process.env.PORT}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import client from "./controller/mqttController";
-
+import indexRoutes from './routes/indexRoutes'
 config()
 
 const app = express()
@@ -9,7 +9,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.set('views', 'src/views')
 app.set('view engine', 'ejs')
-app.use('src/public', express.static('public'))
+app.use('/src/public', express.static('public'))
+app.use('/', indexRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log(`STARTED SERVER ON PORT ${process.env.PORT}`);
