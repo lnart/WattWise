e6f028b110b07317ce09ffdf67a82c48311b9d21
Lennart <115736797+lnart@users.noreply.github.com>
2023-03-20 18:16:41 +0100

    Update index route

--- dist/routes/indexRoutes.js
+++ dist/routes/indexRoutes.js
@@ -3,7 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 let router = (0, express_1.Router)();
 router.get('/', (req, res) => {
-    res.render('test');
+    res.render('test', {
+        liveCount: false,
+        email: false,
+    });
 });
 exports.default = router;
 //# sourceMappingURL=indexRoutes.js.map
--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,10 +1,16 @@
-import { Router } from "express";
+import e, { Router } from "express";
 import { Express, request, response } from "express";
-
+import * as db from '../controller/dbController'
+import * as count from '../controller/countController'
 let router = Router()
 
 router.get('/', (req, res) => {
-    res.render('test')
+    res.render('test', {
+        liveCount: false,
+        email: false,
+
+    })
 })
+
 
 export default router
