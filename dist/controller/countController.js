d24ac7fb23ea2b014eb0a64e7fd7aabae3141b51
Lennart <115736797+lnart@users.noreply.github.com>
2023-04-13 17:13:50 +0200

    functions to parse consumption

--- dist/controller/countController.js
+++ dist/controller/countController.js
@@ -22,10 +22,14 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.calcKwhInEuro = exports.sumArray = exports.getAverageCounts = exports.getAllCountsFromCurrentMonth = exports.createTopic = exports.readYesterdayCount = exports.readLiveCount = exports.calcLiveCount = exports.saveLiveCount = void 0;
+exports.getCountsOfMonth = exports.arrayCountsOfMonth = exports.getArrayOfDaysInMonth = exports.getNextSunday = exports.sumOfWeeklyData = exports.arrayCountsOfWeek = exports.getCountsOfWeek = exports.calcKwhInEuro = exports.sumArray = exports.getAverageCounts = exports.parseAndSortData = exports.extractCountsOfToday = exports.getAllCountsFromCurrentMonth = exports.createTopic = exports.readYesterdayCount = exports.readLiveCount = exports.calcLiveCount = exports.saveLiveCount = void 0;
 const client_1 = require("@prisma/client");
 const db = __importStar(require("./dbController"));
+const dayjs_1 = __importDefault(require("dayjs"));
 const prisma = new client_1.PrismaClient();
 async function saveLiveCount(count, topic) {
     const UID = topic.split('/')[1];
@@ -38,7 +42,8 @@ async function saveLiveCount(count, topic) {
             Date: new Date(),
             Day: new Date().getDate(),
             Month: new Date().getMonth() + 1,
-            Year: new Date().getFullYear()
+            Year: new Date().getFullYear(),
+            WeekOfYear: (0, dayjs_1.default)().week()
         }
     });
 }
@@ -86,12 +91,15 @@ async function createTopic(email, type, array) {
     });
 }
 exports.createTopic = createTopic;
-async function getAllCountsFromCurrentMonth(counter_type) {
+async function getAllCountsFromCurrentMonth(counter_type, email) {
+    const user = await db.findUser(email);
+    const UID = user === null || user === void 0 ? void 0 : user.User_ID;
     const date = new Date();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const allCounts = await prisma.consumption.findMany({
         where: {
+            User_ID: UID,
             Month: month,
             Year: year,
             counter_type: counter_type
@@ -100,16 +108,48 @@ async function getAllCountsFromCurrentMonth(counter_type) {
     return allCounts;
 }
 exports.getAllCountsFromCurrentMonth = getAllCountsFromCurrentMonth;
+async function extractCountsOfToday(array) {
+    const todaysCounts = [];
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1;
+    const today = new Date().getDate();
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]['Year'] === currentYear && array[i]['Month'] === currentMonth && array[i]['Day'] === today) {
+            todaysCounts.push(array[i]['count']);
+        }
+    }
+    return todaysCounts;
+}
+exports.extractCountsOfToday = extractCountsOfToday;
+function parseAndSortData(data) {
+    const currentDate = new Date();
+    const currentDay = currentDate.getUTCDate();
+    const currentMonth = currentDate.getUTCMonth() + 1;
+    const currentYear = currentDate.getUTCFullYear();
+    const sortedData = {};
+    data.forEach(item => {
+        const hour = new Date(item.Date).getUTCHours();
+        if (item.Day === currentDay &&
+            item.Month === currentMonth &&
+            item.Year === currentYear) {
+            if (!sortedData[hour]) {
+                sortedData[hour] = 0;
+            }
+            sortedData[hour] += item.count;
+        }
+    });
+    return sortedData;
+}
+exports.parseAndSortData = parseAndSortData;
 async function getAverageCounts(array) {
     let counts = [];
     const dividor = array.length;
     for (let i = 0; i < array.length; i++) {
         counts.push(array[i].count);
     }
-    console.log(counts);
+    ;
     const sum = await sumArray(counts);
-    console.log(sum);
-    return sum / dividor;
+    return Math.round(sum / dividor);
 }
 exports.getAverageCounts = getAverageCounts;
 async function sumArray(array) {
@@ -122,4 +162,165 @@ async function calcKwhInEuro(count) {
     return Math.round(count * 0.3445);
 }
 exports.calcKwhInEuro = calcKwhInEuro;
+async function getCountsOfWeek(email, counter_type) {
+    const user = await db.findUser(email);
+    const UID = user === null || user === void 0 ? void 0 : user.User_ID;
+    const week = (0, dayjs_1.default)().week();
+    const countsOfWeek = await prisma.consumption.findMany({
+        where: {
+            User_ID: UID,
+            WeekOfYear: week,
+            counter_type: counter_type
+        }
+    });
+    return countsOfWeek;
+}
+exports.getCountsOfWeek = getCountsOfWeek;
+async function arrayCountsOfWeek(email, counter_type) {
+    const counts = await getCountsOfWeek(email, counter_type);
+    const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    const countsByDay = counts.reduce((result, count) => {
+        const dayOfWeek = count.Date.getDay();
+        const dayKey = days[dayOfWeek];
+        //@ts-ignore
+        result[dayKey].push(count.count);
+        return result;
+    }, {
+        sunday: [],
+        monday: [],
+        tuesday: [],
+        wednesday: [],
+        thursday: [],
+        friday: [],
+        saturday: []
+    });
+    return countsByDay;
+}
+exports.arrayCountsOfWeek = arrayCountsOfWeek;
+async function sumOfWeeklyData(data) {
+    const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+    const differences = [];
+    for (let i = 0; i < days.length; i++) {
+        const day = days[i];
+        const counts = data[day];
+        let sum = 0;
+        if (counts.length === 0) {
+            differences.push(0);
+        }
+        else {
+            for (let j = 0; j < counts.length - 1; j++) {
+                sum += counts[j + 1] - counts[j];
+            }
+            differences.push(sum);
+        }
+    }
+    const lastSunday = differences.shift();
+    const nextSundaysCount = await getNextSunday('lola@pafel', 'gas');
+    differences.push(nextSundaysCount);
+    return differences;
+}
+exports.sumOfWeeklyData = sumOfWeeklyData;
+async function getNextSunday(email, counter_type) {
+    const user = await db.findUser(email);
+    const UID = user === null || user === void 0 ? void 0 : user.User_ID;
+    const nextSunday = await prisma.consumption.findFirst({
+        where: {
+            User_ID: UID,
+            counter_type: counter_type,
+            WeekOfYear: (0, dayjs_1.default)().week() + 1
+        },
+        take: 1
+    });
+    if (nextSunday) {
+        const nextSundaysFirstCount = await prisma.consumption.findMany({
+            where: {
+                User_ID: UID,
+                counter_type: 'gas',
+                Day: nextSunday.Day
+            },
+            take: 1
+        });
+        const nextSundaysLastCount = await prisma.consumption.findMany({
+            where: {
+                User_ID: UID,
+                counter_type: 'gas',
+                Day: nextSunday.Day
+            },
+            take: -1
+        });
+        //@ts-ignore
+        return nextSundaysLastCount[0].count - nextSundaysFirstCount[0].count;
+    }
+    else {
+        return 0;
+    }
+}
+exports.getNextSunday = getNextSunday;
+function getArrayOfDaysInMonth() {
+    const upperLimit = (0, dayjs_1.default)().daysInMonth();
+    const days = [];
+    for (let i = 1; i < upperLimit + 1; i++) {
+        days.push(i);
+    }
+    return days;
+}
+exports.getArrayOfDaysInMonth = getArrayOfDaysInMonth;
+async function arrayCountsOfMonth(email, counter_type) {
+    const counts = await getCountsOfMonth(email, counter_type);
+    const days = getArrayOfDaysInMonth();
+    const countsByDay = counts.reduce((result, count) => {
+        const dayOfWeek = count.Date.getDay();
+        const dayKey = days[dayOfWeek];
+        //@ts-ignore
+        result[dayKey].push(count.count);
+        return result;
+    }, {
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: [],
+        6: [],
+        7: [],
+        8: [],
+        9: [],
+        10: [],
+        11: [],
+        12: [],
+        13: [],
+        14: [],
+        15: [],
+        16: [],
+        17: [],
+        18: [],
+        19: [],
+        20: [],
+        21: [],
+        22: [],
+        23: [],
+        24: [],
+        25: [],
+        26: [],
+        27: [],
+        28: [],
+        29: [],
+        30: []
+    });
+    return countsByDay;
+}
+exports.arrayCountsOfMonth = arrayCountsOfMonth;
+async function getCountsOfMonth(email, counter_type) {
+    const user = await db.findUser(email);
+    const UID = user === null || user === void 0 ? void 0 : user.User_ID;
+    const week = (0, dayjs_1.default)().week();
+    const countsOfWeek = await prisma.consumption.findMany({
+        where: {
+            User_ID: UID,
+            Month: new Date().getMonth() + 1,
+            counter_type: counter_type
+        }
+    });
+    return countsOfWeek;
+}
+exports.getCountsOfMonth = getCountsOfMonth;
 //# sourceMappingURL=countController.js.map
--- src/controller/countController.ts
+++ src/controller/countController.ts
@@ -1,21 +1,19 @@
 import { PrismaClient } from "@prisma/client"
 import * as db from "./dbController"
+import dayjs from 'dayjs'
+import { Request,Response } from "express";
 
 const prisma = new PrismaClient()
 
 
 export async function saveLiveCount(count: any, topic: string){
     const UID = topic.split('/')[1]
-    const type = topic.split('/')[0]
-    await prisma.consumption.create({
+    const type = topic.split('/')[0]    
+    await prisma.dailyConsumption.create({
         data: {
-            User_ID: parseFloat(UID),
-            count: parseFloat(count),
-            counter_type: type,
-            Date: new Date(),
-            Day: new Date().getDate(),
-            Month: new Date().getMonth()+1 ,
-            Year: new Date().getFullYear()
+            consumption_date: new Date(),
+            consumption_counts: count,
+            counter_id: parseFloat(UID)
         }
     })
 }
@@ -31,69 +29,104 @@ export async function calcLiveCount(email: string){ //covered
 
 export async function readLiveCount(email: string){ //covered
     const user = await db.findUser(email)
-    const UID = user?.User_ID
-    const liveCount = await prisma.consumption.findFirst({
-        where: {User_ID: UID},
+    const UID = user?.user_id
+    const liveCount = await prisma.dailyConsumption.findFirst({
+        where: {counter_id: UID},
         take: -1
     })
     return liveCount
 }
 
 
-export async function readYesterdayCount(email: string){ //covered
+export async function getAllCountsFromCurrentMonth(counter_type: string, email:string){
     const user = await db.findUser(email)
     const UID = user?.User_ID
-    let liveCount = await readLiveCount(email)
-    //@ts-ignore
-    const day = liveCount.Day
-    const yesterdayCount = await prisma.consumption.findFirst({
-        where: {
-            User_ID: UID,
-            Day: day
-        }
-    })
-    return yesterdayCount
-}
-
-
-export async function createTopic(email: string, type: string, array: any){
-    const user = await db.findUser(email)
-    const userid = user?.User_ID
-    const topic = `${type}/${userid}/live`
-    await prisma.topics.create({
-        data: {
-            name: topic
-        }
-    })
-}
-
-export async function getAllCountsFromCurrentMonth(counter_type: string){
     const date : Date = new Date()
     const month : number = date.getMonth() + 1
     const year : number = date.getFullYear() 
     const allCounts = await prisma.consumption.findMany({
         where: {
+            User_ID:UID,
             Month: month,
             Year: year,
             counter_type: counter_type
         }
     })    
     return allCounts
-} 
+}
 
+
+export async function extractCountsOfToday(array: Array<{User_ID: number, Date: Date, count: number, counter_type: string, Day: number, Month: number, Year: number, WeekOfYear: number, ID: number}>) {
+    const todaysCounts: number[] = [];
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1;
+    const today = new Date().getDate();
+    for (let i = 0; i < array.length; i++) {
+        if (array[i]['Year'] === currentYear && array[i]['Month'] === currentMonth && array[i]['Day'] === today) {
+            todaysCounts.push(array[i]['count']);
+        }
+    }
+    return todaysCounts;
+}
+
+interface DataItem {
+    User_ID: number;
+    Date: string;
+    count: number;
+    counter_type: string;
+    Day: number;
+    Month: number;
+    Year: number;
+    WeekOfYear: number;
+    ID: number;
+  }
+  
+  interface SortedData {
+    [key: number]: number;
+  }
+  
+  export function parseAndSortData(data: DataItem[]): SortedData {
+
+    const currentDate = new Date();
+  
+    const currentDay = currentDate.getUTCDate();
+    const currentMonth = currentDate.getUTCMonth() + 1;
+    const currentYear = currentDate.getUTCFullYear();
+  
+    const sortedData: SortedData = {};
+  
+    data.forEach(item => {
+      const hour = new Date(item.Date).getUTCHours();
+  
+      if (
+        item.Day === currentDay &&
+        item.Month === currentMonth &&
+        item.Year === currentYear
+      ) {
+        if (!sortedData[hour]) {
+          sortedData[hour] = 0;
+        }
+  
+        sortedData[hour] += item.count;
+      }
+    });
+  
+    return sortedData;
+  }
+
+  
+  
+  
 
 export async function getAverageCounts(array: any) {
     let counts:any = []
     const dividor:number = array.length
     for(let i = 0; i<array.length; i++){
         counts.push(array[i].count)
-    }
-    console.log(counts);
-    
+    };
     const sum = await sumArray(counts)
-    console.log(sum);
     
-    return sum / dividor
+    return Math.round(sum / dividor)
 }
 
 export async function sumArray(array: any[]){
@@ -104,4 +137,171 @@ export async function sumArray(array: any[]){
 
 export async function calcKwhInEuro(count: number){
     return Math.round(count*0.3445)
+}
+
+export async function getCountsOfWeek(email:string, counter_type:string){
+    const user = await db.findUser(email)
+    const UID = user?.User_ID
+    const week = dayjs().week()
+    
+    const countsOfWeek = await prisma.consumption.findMany({
+        where:{
+            User_ID: UID,
+            WeekOfYear: week,
+            counter_type:counter_type
+        }
+    })
+    return countsOfWeek
+}
+
+export async function arrayCountsOfWeek(email: string, counter_type:string) {
+    const counts = await getCountsOfWeek(email, counter_type);
+    const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  
+    const countsByDay = counts.reduce((result, count) => {
+      const dayOfWeek = count.Date.getDay();
+      const dayKey = days[dayOfWeek];
+      //@ts-ignore
+      result[dayKey].push(count.count);
+      return result;
+    }, {
+      sunday: [],
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: [],
+      saturday: []
+    });
+    return countsByDay;
+  }
+
+  export async function sumOfWeeklyData(data: { [x: string]: never[]; }) {
+    const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  const differences = [];
+  
+  for (let i = 0; i < days.length; i++) {
+    const day = days[i];
+    const counts = data[day];
+    let sum = 0;
+    
+    if (counts.length === 0) {
+      differences.push(0);
+    } else {
+      for (let j = 0; j < counts.length - 1; j++) {
+        sum += counts[j + 1] - counts[j];
+      }
+      differences.push(sum);
+    }
+  }
+  const lastSunday = differences.shift()
+  const nextSundaysCount = await getNextSunday('lola@pafel', 'gas')
+  differences.push(nextSundaysCount)
+  return differences;
+
+  }
+
+export async function getNextSunday(email:string, counter_type:string){
+    const user = await db.findUser(email)
+    const UID = user?.User_ID
+    const nextSunday = await prisma.consumption.findFirst({
+        where:{
+            User_ID:UID,
+            counter_type:counter_type,
+            WeekOfYear: dayjs().week()+1
+        },
+        take: 1
+    })
+    if(nextSunday){
+        const nextSundaysFirstCount = await prisma.consumption.findMany({
+            where: {
+                User_ID: UID,
+                counter_type: 'gas',
+                Day: nextSunday.Day
+            },
+            take: 1
+        })
+        const nextSundaysLastCount = await prisma.consumption.findMany({
+            where: {
+                User_ID: UID,
+                counter_type: 'gas',
+                Day: nextSunday.Day
+            },
+            take: -1
+        })
+        //@ts-ignore
+        return nextSundaysLastCount[0].count - nextSundaysFirstCount[0].count
+    }else{
+        return 0
+    }
+}
+
+export function getArrayOfDaysInMonth(){
+    const upperLimit = dayjs().daysInMonth()
+    const days = []
+    for(let i = 1; i<upperLimit+1; i++){
+        days.push(i)
+    }
+    return days
+}
+
+export async function arrayCountsOfMonth(email: string, counter_type:string) {
+    const counts = await getCountsOfMonth(email, counter_type);
+    const days = getArrayOfDaysInMonth();
+  
+    const countsByDay = counts.reduce((result, count) => {
+      const dayOfWeek = count.Date.getDay();
+      const dayKey = days[dayOfWeek];
+      //@ts-ignore
+      result[dayKey].push(count.count);
+      return result;
+    }, {
+      1: [],
+      2: [],
+      3: [],
+      4: [],
+      5: [],
+      6: [],
+      7: [],
+      8: [],
+      9: [],
+      10: [],
+      11: [],
+      12: [],
+      13: [],
+      14: [],
+      15: [],
+      16: [],
+      17: [],
+      18: [],
+      19: [],
+      20: [],
+      21: [],
+      22: [],
+      23: [],
+      24: [],
+      25: [],
+      26: [],
+      27: [],
+      28: [],
+      29: [],
+      30: []
+
+    });
+    return countsByDay;
+  }
+
+  export async function getCountsOfMonth(email:string, counter_type:string){
+    const user = await db.findUser(email)
+    const UID = user?.User_ID
+    const week = dayjs().week()
+    
+    const countsOfWeek = await prisma.consumption.findMany({
+        where:{
+            User_ID: UID,
+            Month: new Date().getMonth()+1,
+            counter_type:counter_type
+        }
+    })
+    return countsOfWeek
 }
