0c54e1a8ef15be30f120fd8b85d92859648cf115
Lennart <115736797+lnart@users.noreply.github.com>
2023-04-24 14:59:05 +0200

    call execution functions

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,28 @@ import express from "express";
 import { config } from "dotenv";
 import indexRoutes from './routes/indexRoutes'
 import loginRoutes from './routes/loginRoutes'
+import deletionRoutes from './routes/deletionRoutes'
 import * as myMqttClient from './controller/clientController'
+import * as weather from './controller/weatherApiController'
+import * as db from './controller/dbController'
+import * as count from './controller/countController'
+import * as execute from './controller/executeController'
+import cookieParser from "cookie-parser";
+import WeekOfYear from 'dayjs/plugin/weekOfYear'
+import isoWeek from 'dayjs/plugin/isoWeek'
+import dayjs from '../src/controller/customLocale'
+import timezone from "dayjs/plugin/timezone";
+import utc from 'dayjs/plugin/utc'
+import { getNextDay } from "../src/controller/customLocale";
+import * as reader from './controller/readController'
+import { Prisma, PrismaClient } from "@prisma/client";
+
+
+dayjs.extend(utc)
+dayjs.extend(WeekOfYear)
+dayjs.extend(isoWeek)
+dayjs.extend(timezone)
+ 
 config()
 
 const app = express()
@@ -14,10 +35,29 @@ app.set('view engine', 'ejs')
 app.use('/public', express.static('public'))
 app.use(indexRoutes)
 app.use(loginRoutes)
+app.use(deletionRoutes)
 mqttClient
+app.use(cookieParser())
+const prisma = new PrismaClient()
 
-myMqttClient.generateValue()
+execute.executeEveryNewHour()
+setInterval(execute.executeEveryNewHour, 1000 * 60 * 60) 
 
+count.saveWeeklyCounts()
+setInterval(execute.executeSaveWeeklyCountsDaily, 1000)
+
+execute.executeSaveMonthlyCountsDaily()
+setInterval(execute.executeSaveMonthlyCountsDaily, 1000)
+
+async function x() {
+ 
+ const dongs = await reader.getyAxisYear(2, 'gas')
+   console.log(dongs); 
+  }
+x()
+ 
+ 
 app.listen(process.env.PORT, () => {
-    console.log(`STARTED SERVER ON PORT ${process.env.PORT}`);
+    console.log(`STARTED SERVER ON PORT ${process.env.PORT}`); 
 })
+
