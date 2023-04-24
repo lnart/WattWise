742e875ee4c9c47aa1e8d46d9bb64febe6bbc6a7
Lennart <115736797+lnart@users.noreply.github.com>
2023-04-24 14:53:52 +0200

    add cutsomization to dayjs library

---
+++ src/controller/customLocale.ts
@@ -0,0 +1,25 @@
+// customLocale.ts
+
+import dayjs, { Dayjs } from 'dayjs';
+import WeekOfYear from 'dayjs/plugin/weekOfYear'
+import Isoweek from 'dayjs/plugin/isoWeek'
+const customLocale = {
+  name: 'customLocale',
+  weekStart: 1,
+};
+//@ts-ignore
+dayjs.locale(customLocale); 
+
+export function getNextDay(date:Date){
+  const nextDay = new Date(date.getFullYear(), date.getMonth(), date.getDate()+1)
+  if (nextDay.getMonth() !== date.getMonth()) {
+    nextDay.setDate(1);
+    nextDay.setMonth(date.getMonth() + 1);
+  }
+  return nextDay
+}
+
+//@ts-ignore
+export default dayjs;
+
+
