52c5b3146f7bc0001f44c5e41e7818e501d71fc6
Lennart <115736797+lnart@users.noreply.github.com>
2023-04-26 16:51:27 +0200

    add extract user id and type function

---
+++ src/helpers/extractHelpers.ts
@@ -0,0 +1,13 @@
+
+
+
+
+export function extractUserIdFromTopic(topic:string){
+    const UID = topic.split('/')[1]
+    return UID
+}
+
+export function extractCounterTypeFromTopic(topic:string){
+    const type:string = topic.split('/')[0]
+    return type
+}
