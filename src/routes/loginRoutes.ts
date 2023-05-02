56239fc67e2d2d38923fe96c2a02a61d286c08d7
Lennart <115736797+lnart@users.noreply.github.com>
2023-05-02 12:29:00 +0200

    refactor, add error handling, delete unnecessary code

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -13,47 +13,61 @@ let router = Router()
 router.use(cookieParser());
 
   
-router.get("/login", (req, res) => {    
-    res.render('login')
+router.get("/login", (req, res) => {
+    try {
+        res.render('login')
+    } catch (error) {
+        console.error(error)
+    }    
   });
   
 
-  router.get("/logout", (req, res) => {    
-    return res
-      .clearCookie('email')
-      .clearCookie("access_token")
-      .status(200)
-      .redirect('/login')
+  router.get("/logout", (req, res) => {
+      try {
+        return res
+          .clearCookie('email')
+          .clearCookie("access_token")
+          .status(200)
+          .redirect('/login')
+    } catch (error) {
+        console.error(error)
+    }    
   });
   
 
 router.post('/register', async (req, res) => {
-    if(await findUser(req.body.email) === null){
-        let salt = await bcrypt.genSalt()
-        let newUser = await createUser({
-            username: req.body.firstname,
-            Email: req.body.email,
-            Zipcode: parseFloat(req.body.zipcode),
-            Password: await bcrypt.hash(req.body.password, salt),
-        })
-        res.redirect('/login')
-    }else{
-        res.redirect('/login')
+    try {
+        if(await findUser(req.body.email) === null){
+            let salt = await bcrypt.genSalt()
+            let newUser = await createUser({
+                username: req.body.firstname,
+                Email: req.body.email,
+                Zipcode: parseFloat(req.body.zipcode),
+                Password: await bcrypt.hash(req.body.password, salt),
+            })
+            res.redirect('/login')
+        }else{
+            res.redirect('/login')
+        }
+        
+    } catch (error) {
+        console.error(error)
     }
 })
 
 router.get('/register', (req,res) =>{
-    res.render('register', {
-        message: false
-    })
+    try {
+        res.render('register', {
+            message: false
+        })
+        
+    } catch (error) {
+        console.error(error)
+    }
 })
 
 router.post('/login', async(req, res) => {
-    const user = await findUser(req.body.email)
-    // const role = user?.Role
-    
     try{
-        
         if(await login.logInUser(req, res) && process.env.ACCESS_TOKEN_SECRET){
             const token = Jwt.sign({email: req.body.email}, process.env.ACCESS_TOKEN_SECRET)
             res
@@ -68,8 +82,6 @@ router.post('/login', async(req, res) => {
 })
 
 router.get('/user', wt.authorization, async(req, res) => {
-    console.log(req.cookies);
-    
     res.json({cookies: req.cookies})
 })
 
