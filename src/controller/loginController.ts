5af401bc2157f255b89f5f4d3c80d7d826fbf6f9
Lennart <115736797+lnart@users.noreply.github.com>
2023-05-02 11:37:42 +0200

    refactor, add error handling

--- src/controller/loginController.ts
+++ src/controller/loginController.ts
@@ -4,31 +4,47 @@ import { config } from 'dotenv'
 config()
 
 export async function checkIfUserExist(email: string){
-    if(await db.findUser(email)){
-        return true
-    }else{
-        return false
+    try {
+        if(await db.findUser(email)){
+            return true
+        }else{
+            return false
+        }
+    
+    } catch (error) {
+        console.error(error)
+        throw new Error('User was not found')
     }
 }
 
 export async function compareUserCredentials(email: string, password: string){
-    const user = await db.findUser(email)
-    const hashedPassword = user?.password    
-    //@ts-ignore
-    const match = bcrypt.compare(password, hashedPassword)
-    if(match) return true
-    return false
+    try {
+        const user = await db.findUser(email)
+        const hashedPassword = user?.password    
+        //@ts-ignore
+        const match = bcrypt.compare(password, hashedPassword)
+        if(match) return true
+        return false
+        
+    } catch (error) {
+        console.error(error)
+    }
     
 }
 
 export async function logInUser(req:any, res:any){
-    const user = await db.findUser(req.body.email)
-    if(await checkIfUserExist(req.body.email) === false){        
-        return res.redirect('/login')
-    }else if(await checkIfUserExist(req.body.email) && await compareUserCredentials(req.body.email, req.body.password)){        
-        return true
-    }else{        
-        return res.redirect('/login')
+    try {
+        const user = await db.findUser(req.body.email)
+        if(await checkIfUserExist(req.body.email) === false){        
+            return res.redirect('/login')
+        }else if(await checkIfUserExist(req.body.email) && await compareUserCredentials(req.body.email, req.body.password)){        
+            return true
+        }else{        
+            return res.redirect('/login')
+        }
+        
+    } catch (error) {
+        console.error(error)
     }
 }
 
