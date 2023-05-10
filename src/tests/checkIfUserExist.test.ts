fc0ad322e1e443711437f32df5e73ce6d35f308d
Lennart <115736797+lnart@users.noreply.github.com>
2023-05-10 17:40:17 +0200

    testing: add tests for the loginController

--- src/tests/checkIfUserExist.test.ts
+++ src/tests/checkIfUserExist.test.ts
@@ -1,41 +1,84 @@
 import { describe, test, vi , expect, afterEach, beforeEach} from "vitest";
+
 import { checkIfUserExist } from "../controller/loginController";
+
 import { checkServerIdentity } from "tls";
 
-describe('checkIfUserExist', () => {
+vi.mock('../controller/dbController.ts', async () => {
+    function findUserMock(){}
+    return {
+        findUser: findUserMock
+    }
+})
 
-    afterEach(async() => {
-        vi.restoreAllMocks()
+describe('checkIfUserExist', () => {
+    const findUserMock = vi.fn()
+    
+    beforeEach(() => {
+        vi.resetAllMocks()
     })
+
     //the first one passes
-    test('should return true if user exist', async() => {
-        vi.mock('../controller/dbController.ts', async() => {
-            return {
-                findUser: async() => {
-                   return {email:'test@example.com', name: 'test user', password: '0000'}
-                }
-            }
-        } ) 
+    test('should return true if user exists', async () => {
+        findUserMock.mockReturnValueOnce({email: 'test@example.com', name: 'test user', password: '0000'})
+        
         const result = checkIfUserExist('jkhdkf')
+
         expect(result).toBeTruthy()
     })
+
     //the second fails because of the return value of the first mock
-    test('should return null if user does not exist', async()=> {
-        vi.mock('../controller/dbController.ts', async() => {
-            return {
-                findUser: async() => {
-                    return null
-                }
-            }
-        })
+    test('should return null if user does not exist', async () => {
+        findUserMock.mockReturnValueOnce(null)
+
         const result = await checkIfUserExist('lola@pafel')
+
         expect(result).toBeFalsy()
     })
-    
+
+    afterEach(async () => {
+        vi.restoreAllMocks()
+    })
 })
 
 
+// import { describe, test, vi , expect, afterEach, beforeEach} from "vitest";
+// import { checkIfUserExist } from "../controller/loginController";
+ 
 
+// describe('checkIfUserExist', () => {
+//     afterEach(async() => {
+//         vi.restoreAllMocks()
+//     })
     
+//     //the first one passes
+//     test('should return true if user exist', async() => {
+//         vi.mock('../controller/dbController.ts', async() => {
+//             return {
+//                 findUser: async() => {
+//                    return {email:'test@example.com', name: 'test user', password: '0000'}
+//                 }
+//             }
+//         } ) 
+//         const result = checkIfUserExist('jkhdkf')
+//         expect(result).toBeTruthy()
+//     })
+//     //the second fails because of the return value of the first mock
+//     test('should return null if user does not exist', async()=> {
+//         vi.mock('../controller/dbController.ts', async() => {
+//             return {
+//                 findUser: async() => {
+//                     return null
+//                 }
+//             }
+//         })
+//         const result = await checkIfUserExist('lola@pafel')
+//         expect(result).toBeFalsy()
+//     })
+    
+// })
+
+
+
 
 
