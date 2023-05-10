08e02f34e03e2b589c7ada695b28f1e2bcbab0d4
Lennart <115736797+lnart@users.noreply.github.com>
2023-05-10 17:40:46 +0200

    testing: add test for the counterController

--- src/tests/count.test.ts
+++ src/tests/count.test.ts
@@ -1,35 +1,23 @@
-
-// import * as count from '../controller/countController'
-// import * as db from '../controller/dbController'
-// import { describe, expect, vi, test } from 'vitest'
-
-
-// describe('readLiveCount', () => {
-//     test('returns number from counter', async () => {
-        
-//         const result = await count.readLiveCount('lola@pafel')
-
-//         const resultCount = result!.count  
-    
-//         expect(resultCount).toBeTypeOf('number')
-//     })
-
-//     test('returns number from yesterdays count', async() => {
-//         let result = await count.readYesterdayCount('lola@pafel')
-//         //@ts-ignore
-//         expect(result).toHaveProperty('User_ID').toHaveProperty('Date').toHaveProperty('count').toHaveProperty('counter_type').toHaveProperty('Day').toHaveProperty('Month').toHaveProperty('Year')
-//     })
-
-//     test('returns calculated live count', async() => {
-//         let result = await count.calcLiveCount('lola@pafel')
-//         expect(result).toBeTypeOf('number')
-//     })
-// })
+import {afterEach, beforeEach,expect, vi, test} from 'vitest'
+import { saveLiveCount } from '../controller/countController'
+import { findCounter } from '../helpers/dbHelpers';
 
 
-
-
-
-
-
-
+test("should upate counter if it finds one", async() => {
+    vi.mock('../helpers/dbHelpers.ts', async () => {
+      return {
+        findCounter:async () => {
+          return {
+            counter_id: 999,
+            user_id: 999,
+            type: 'water',
+            count: 1,
+            timestamp: '2023-05-07 22:00:00'
+          }
+        }
+      }
+    })
+    const startOfDay = '2023-05-07T22:00:00.000Z'
+    const result = await saveLiveCount('999', 'water', startOfDay, '1')
+    expect(result).toEqual('counter 999 was updated')
+  }) 
